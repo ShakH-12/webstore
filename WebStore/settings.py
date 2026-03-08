0ffdbc336bf0ed95d655f6b3c8719c3952428298
ShakH-12 <shakh8778@gmail.com>
2026-03-08 08:41:27 +0000

    fix

--- WebStore/settings.py
+++ WebStore/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 3
+    "PAGE_SIZE": 10
 }
 
 SIMPLE_JWT = {
@@ -201,7 +201,6 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = "DENY"
 
-"""
 LOGGING = {
     "version": 1,
     "disable_existing_logger": False,
@@ -220,4 +219,3 @@ LOGGING = {
         }
     }
 }
-"""
